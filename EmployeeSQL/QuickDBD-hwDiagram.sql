93a5cdf7ae92710bfa8b5ca0d77309a1fb9d1c8d
MuratCKOC <muratcagrikoc@gmail.com>
2020-11-23 21:44:56 -0500

    relationship sql

---
+++ EmployeeSQL/QuickDBD-hwDiagram.sql
@@ -0,0 +1,70 @@
+﻿CREATE TABLE "Employee" (
+    "emp_no" varchar(250)   NOT NULL,
+    "emp_title" varchar(250)   NOT NULL,
+    "first_name"  varchar(250) NOT NULL,
+    "last_name"  varchar(250)  NOT NULL,
+    "birth_date" date   NOT NULL,
+    "sex"  varchar(20)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_Employee" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" varchar(250)   NOT NULL,
+    "dept_no" varchar(250)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "departments" (
+    "dept_no" varchar(250)   NOT NULL,
+    "dept_name" varchar(250)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "emp_no" varchar(250)   NOT NULL,
+    "dept_no" varchar(250)   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" varchar(250)   NOT NULL,
+    "title" varchar(250)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" varchar(250) NOT NULL,
+    "salary" int NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+ALTER TABLE "Employee" ADD CONSTRAINT "fk_Employee_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "salaries" ("emp_no");
+
+ALTER TABLE "Employee" ADD CONSTRAINT "fk_Employee_emp_title" FOREIGN KEY("emp_title")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employee" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employee" ("emp_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
